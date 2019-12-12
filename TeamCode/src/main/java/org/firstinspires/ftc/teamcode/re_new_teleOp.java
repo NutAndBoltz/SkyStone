90c793cbad2876f9e3b54ac173632f30db87bebd
Ivory0321@gmail.com <korivory@gmail.com>
2019-12-12 16:34:49 -0600

    minor fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/re_new_teleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/re_new_teleOp.java
@@ -4,55 +4,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "re_new_teleOp")
 public class re_new_teleOp extends robotMovements {
-    private double motorFLspeed=0;
-    private double motorFRspeed=0;
-    private double motorBLspeed=0;
-    private double motorBRspeed=0;
+
     private float G1leftStickX;
     private float G1rightStickX;
     private float G1rightStickY;
     private float G1leftStickY;
-    private float G1left_trigger;
-    private float G1right_trigger;
-    private boolean G1buttonB;
-
-    void adjust()
-    {
-        if(motorFLspeed>1)
-        {
-            motorFLspeed=1;
-        }
-        if(motorFRspeed>1)
-        {
-            motorFRspeed=1;
-        }
-        if(motorBLspeed>1)
-        {
-            motorBLspeed=1;
-        }
-        if(motorBRspeed>1)
-        {
-            motorBRspeed=1;
-        }
-
-        if(motorFLspeed<1)
-        {
-            motorFLspeed=-1;
-        }
-        if(motorFRspeed<1)
-        {
-            motorFRspeed=-1;
-        }
-        if(motorBLspeed<1)
-        {
-            motorBLspeed=-1;
-        }
-        if(motorBLspeed<1)
-        {
-            motorBRspeed=-1;
-        }
-    }
-
 
 
 
@@ -81,7 +37,6 @@ public class re_new_teleOp extends robotMovements {
     @Override
     public void runOpMode() throws InterruptedException {
         super.runOpMode();
-
         setMode(TELEOP);
 
         while(opModeIsActive())
@@ -90,9 +45,6 @@ public class re_new_teleOp extends robotMovements {
             G1rightStickX =-gamepad1.right_stick_x;
             G1rightStickY = -gamepad1.right_stick_y;
             G1leftStickY = -gamepad1.left_stick_y;
-            //G1left_trigger = gamepad1.left_trigger;
-            //G1right_trigger= gamepad1.right_trigger;
-            //G1buttonB = gamepad1.b;
             float dpadThreshold = 0.2f;
             if(gamepad1.y)
             {
@@ -109,43 +61,33 @@ public class re_new_teleOp extends robotMovements {
                 //move claw up
                 robot.foundationClaw.setPosition(1);
             }
-            while(G1leftStickY>0.3)
+            if(G1leftStickY>0.3)
             {
                 moveForward();
 
             }
-            while(G1leftStickY<0.3)
+            if(G1leftStickY<0.3)
             {
                 moveBackward();
             }
-            while(G1leftStickX<-0.3)
+            if(G1leftStickX<-0.3)
             {
                 moveLeft(0);
             }
-            while(G1leftStickX>0.3)
+            if(G1leftStickX>0.3)
             {
                 moveRight(0);
             }
-            while(G1rightStickX>0.3)
+            if(G1rightStickX>0.3)
             {
                 turnright(G1rightStickX);
             }
-            while(G1rightStickX<-0.3)
+            if(G1rightStickX<-0.3)
             {
                 turnleft(G1rightStickX);
             }
 
-
-            robot.motorFL.setPower(motorFLspeed);
-            robot.motorFR.setPower(motorFRspeed);
-            robot.motorBL.setPower(motorBLspeed);
-            robot.motorBR.setPower(motorBRspeed);
-
             telemetry.addData("Status", "Running");
-            telemetry.addData("motorFL",motorFLspeed);
-            telemetry.addData("motorFR",motorFRspeed);
-            telemetry.addData("motorBL",motorBLspeed);
-            telemetry.addData("motorBR",motorBRspeed);
             telemetry.update();
         }
 
